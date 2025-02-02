f3a4e463eb18ebacd68b6b93c52d57700c1e035b
tonybarr1611 <antbarrantes@estudiantec.cr>
2025-02-02 03:07:41 -0600

    Movie search component
    Card display using the search term given. Also gives the option to filter the data using genre, year, popularity and even sort it both asc and desc.

---
+++ frontend/moviesitory/src/app/pages/movies-search/movies-search.component.ts
@@ -0,0 +1,92 @@
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { MoviesitoryService } from '../../services/moviesitory.service';
+import { MovieRequestResponse } from '../../models/movie.model';
+import { CardComponent } from '../../components/card/card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'movies-search',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CardComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatPaginatorModule,
+  ],
+  templateUrl: './movies-search.component.html',
+  styleUrl: './movies-search.component.scss',
+})
+export class MoviesSearchComponent implements OnInit {
+  @Input() searchTerm: string = '';
+  @Input() results: number = 4;
+  @Input() limit: number = 4;
+  movies: MovieRequestResponse = {
+    movies: [],
+    totalMovies: 0,
+    totalPages: 0,
+    currentPage: 0,
+  };
+  currentPage = 1;
+  currentLimit = this.limit;
+  filterForm!: FormGroup;
+
+  constructor(
+    private moviesitoryService: MoviesitoryService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe((params) => {
+      if (params['searchTerm']) {
+        this.searchTerm = params['searchTerm'];
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.filterForm = this.fb.group({
+      genre: [''],
+      year: [''],
+      popularity: [''],
+      sortBy: ['title'],
+      order: ['asc'],
+    });
+    this.updateSearch();
+  }
+
+  pageChange(event: PageEvent) {
+    console.log(event);
+    this.currentPage = event.pageIndex + 1;
+    this.currentLimit = event.pageSize;
+    this.updateSearch();
+  }
+
+  updateSearch() {
+    this.moviesitoryService
+      .getMoviesService()
+      .getMovies({
+        search: this.searchTerm,
+        sortBy: this.filterForm.value.sortBy,
+        limit: this.currentLimit,
+        order: this.filterForm.value.order,
+        page: this.currentPage,
+        genre: this.filterForm.value.genre,
+        year: this.filterForm.value.year,
+        popularity: this.filterForm.value.popularity / 0.5,
+      })
+      .subscribe((movies) => {
+        this.movies.movies = [];
+        this.movies = movies;
+      });
+  }
+}
